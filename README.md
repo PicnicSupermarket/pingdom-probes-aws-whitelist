60e9d030a497542df73ec35400a37ffad139903a
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2017-03-13 11:59:12 +0100

    Introduce "Pingdom IPs to AWS SGs" script

---
+++ README.md
@@ -0,0 +1,94 @@
+### Sync Pingdom probe IPs to AWS security groups
+
+A script for synchronizing [AWS security
+group](https://docs.aws.amazon.com/opsworks/latest/userguide/best-practices-groups.html)
+ingress rules with the published list of [Pingdom probe IPv4
+addresses](https://my.pingdom.com/probes/ipv4).  This script is inspired by and
+an alternative to the following projects:
+- [`akremer/pingdom-ec2-security`](https://github.com/akremer/pingdom-ec2-security)
+- [`degiere/pingdom-ec2-security-group-updater`](https://github.com/degiere/pingdom-ec2-security-group-updater)
+
+Advantages of this script over either or both of the scripts listed above:
+- It is not affected by the AWS limit of 50 ingress rules per SG, as multiple
+  security groups may (and should!) be provided.
+- It does not unnecessarily modify the security groups upon repeated
+  invocations.
+- It drops obsolete ingress rules.
+
+#### Usage
+
+By default the script adds a TCP port 80 ingress rule for each Pingdom probe
+IP. It modifies only listed security groups. For all supported options, run the
+script with `--help`:
+
+```
+$ ./sync-pingdom-ec2-security-groups.py --help
+usage: sync-pingdom-ec2-security-groups.py [-h] [--whitelist WHITELIST]
+                                           [--protocol {icmp,tcp,udp}]
+                                           [--port PORT]
+                                           security-group [security-group ...]
+
+positional arguments:
+  security-group        One of the security groups to be updated
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --whitelist WHITELIST
+                        The URL at which the IP whitelist is located; must
+                        contain one one IP per line
+  --protocol {icmp,tcp,udp}
+                        The protocol used by the Pingdom probe
+  --port PORT           The port on which Pingdom probes
+```
+
+Note that your environment must be configured to provide valid AWS credentials.
+See the [Boto
+documentation](https://boto3.readthedocs.io/en/latest/guide/configuration.html)
+or the [AWS CLI
+documentation](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html)
+for instructions on how to set this up.
+
+#### Example invocation
+
+The following run shows the effect of synchronizing a set of three security
+groups (with anonymized IDs) after Pingdom abandoned four IPs since the script
+was last run:
+
+
+```
+$ ./sync-pingdom-ec2-security-groups.sh sg-12345678 sg-23456789 sg-34567890
+Dropping from SG sg-12345678: Permission tcp:78.31.69.179/32:80-80
+Dropping from SG sg-12345678: Permission tcp:76.72.171.180/32:80-80
+Dropping from SG sg-12345678: Permission tcp:158.58.173.160/32:80-80
+Dropping from SG sg-12345678: Permission tcp:72.46.140.186/32:80-80
+Adding to SG sg-12345678: Permission tcp:54.70.202.58/32:80-80
+Adding to SG sg-12345678: Permission tcp:52.197.224.235/32:80-80
+Adding to SG sg-12345678: Permission tcp:52.63.164.147/32:80-80
+Adding to SG sg-12345678: Permission tcp:23.111.152.74/32:80-80
+Dropping from SG sg-23456789: Permission tcp:54.70.202.58/32:80-80
+Dropping from SG sg-23456789: Permission tcp:52.197.224.235/32:80-80
+Dropping from SG sg-23456789: Permission tcp:52.63.164.147/32:80-80
+Dropping from SG sg-23456789: Permission tcp:23.111.152.74/32:80-80
+Adding to SG sg-23456789: Permission tcp:52.63.142.2/32:80-80
+Adding to SG sg-23456789: Permission tcp:52.209.34.226/32:80-80
+Adding to SG sg-23456789: Permission tcp:178.255.154.2/32:80-80
+Adding to SG sg-23456789: Permission tcp:54.68.48.199/32:80-80
+Dropping from SG sg-34567890: Permission tcp:52.63.142.2/32:80-80
+Dropping from SG sg-34567890: Permission tcp:52.209.34.226/32:80-80
+Dropping from SG sg-34567890: Permission tcp:178.255.154.2/32:80-80
+Dropping from SG sg-34567890: Permission tcp:54.68.48.199/32:80-80
+SUCCESS
+```
+
+Running the script once more does not further modify the security groups:
+```
+$ ./sync-pingdom-ec2-security-groups.sh sg-12345678 sg-23456789 sg-34567890
+SUCCESS
+```
+
+#### Contributing
+
+Contributions are welcome! Feel free to file an
+[issue](https://github.com/PicnicSupermarket/pingdom-probes-aws-whitelist/issues/new)
+or open a [pull
+request](https://github.com/PicnicSupermarket/pingdom-probes-aws-whitelist/compare).
---
+++ sync-pingdom-ec2-security-groups.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import argparse
+import boto3
+import requests
+
+# The maximum number of rules that may be contained in a single AWS security
+# group.
+MAX_SG_PERM_COUNT = 50
+
+class Permission(object):
+    def __init__(self, protocol, cidr_ip, from_port, to_port):
+        self.protocol = protocol
+        self.cidr_ip = cidr_ip
+        self.from_port = from_port
+        self.to_port = to_port
+
+    def as_dict(self):
+        return {
+            'IpProtocol': self.protocol,
+            'CidrIp': self.cidr_ip,
+            'FromPort': self.from_port,
+            'ToPort': self.to_port
+        }
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
+
+    def __repr__(self):
+        return 'Permission({0.protocol!r}, {0.cidr_ip!r}, {0.from_port!r}, {0.to_port!r})'.format(self)
+
+    def __str__(self):
+        return 'Permission {0.protocol}:{0.cidr_ip}:{0.from_port}-{0.to_port}'.format(self)
+
+class PingdomSecurityGroup(object):
+    def __init__(self, sg):
+        self.sg = sg
+
+    def update_permissions(self, target_perms):
+        active_perms = self.get_permissions()
+
+        self.drop_permissions(active_perms - target_perms)
+        active_perms &= target_perms
+        target_perms -= active_perms
+
+        new_perms = set(list(target_perms)[:MAX_SG_PERM_COUNT - len(active_perms)])
+        self.add_permissions(new_perms)
+        target_perms -= new_perms
+
+    def get_permissions(self):
+        # XXX: Assumes that "IpRanges" contains only CIDR entries, not SG IDs.
+        return set(Permission(p['IpProtocol'], r['CidrIp'], p['FromPort'], p['ToPort']) for p in self.sg.ip_permissions for r in p['IpRanges'])
+
+    def drop_permissions(self, permissions):
+        for p in permissions:
+            print('Dropping from SG {0}: {1}'.format(self.sg.id, p))
+            self.sg.revoke_ingress(**p.as_dict())
+
+    def add_permissions(self, permissions):
+        for p in permissions:
+            print('Adding to SG {0}: {1}'.format(self.sg.id, p))
+            self.sg.authorize_ingress(**p.as_dict())
+
+class SecurityGroupUpdater(object):
+    def __init__(self, whitelist, protocol, from_port, to_port, security_groups):
+        self.whitelist = whitelist
+        self.protocol = protocol
+        self.from_port = from_port
+        self.to_port = to_port
+        self.security_groups = security_groups
+
+    def run(self):
+        permissions = self.get_target_permissions()
+        self.configure_permissions(permissions)
+        if permissions:
+            print('WARNING: {0} permissions could not be configured; please specify more security groups.'.format(len(permissions)))
+        else:
+            print('SUCCESS')
+
+    def get_target_permissions(self):
+        return set(map(self.create_permission, self.get_ips()))
+
+    def get_ips(self):
+        r = requests.request('GET', self.whitelist, timeout=10)
+        r.raise_for_status()
+        return set(r.iter_lines())
+
+    def create_permission(self, ip):
+        return Permission(self.protocol, '{0}/32'.format(ip), self.from_port, self.to_port)
+
+    def configure_permissions(self, permissions):
+        ec2 = boto3.resource('ec2')
+        for sg_id in self.security_groups:
+            sg = PingdomSecurityGroup(ec2.SecurityGroup(sg_id))
+            sg.update_permissions(permissions)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--whitelist',
+        default='https://my.pingdom.com/probes/ipv4',
+        help='The URL at which the IP whitelist is located; must contain one one IP per line')
+    parser.add_argument(
+        '--protocol',
+        choices=('icmp', 'tcp', 'udp'),
+        default='tcp',
+        help='The protocol used by the Pingdom probe')
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=80,
+        help='The port on which Pingdom probes')
+    parser.add_argument(
+        'security-group',
+        nargs='+',
+        help='One of the security groups to be updated')
+    args = parser.parse_args()
+
+    updater = SecurityGroupUpdater(args.whitelist, args.protocol, args.port, args.port, getattr(args, 'security-group'))
+    updater.run()
+
+if __name__ == '__main__':
+    main()
