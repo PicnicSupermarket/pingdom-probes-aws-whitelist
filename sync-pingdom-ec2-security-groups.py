c43b4e5babe7144681913540da036aa68bbdec39
lovelycuppatea <lovelycuppatea@users.noreply.github.com>
2017-05-22 16:27:58 +0100

    Allow overriding the target AWS region (#3)

--- sync-pingdom-ec2-security-groups.py
+++ sync-pingdom-ec2-security-groups.py
@@ -71,7 +71,8 @@ class PingdomSecurityGroup(object):
             self.sg.authorize_ingress(**p.as_dict())
 
 class SecurityGroupUpdater(object):
-    def __init__(self, whitelist, protocol, from_port, to_port, security_groups):
+    def __init__(self, region, whitelist, protocol, from_port, to_port, security_groups):
+        self.region = region
         self.whitelist = whitelist
         self.protocol = protocol
         self.from_port = from_port
@@ -98,13 +99,17 @@ class SecurityGroupUpdater(object):
         return Permission(self.protocol, '{0}/32'.format(ip), self.from_port, self.to_port)
 
     def configure_permissions(self, permissions):
-        ec2 = boto3.resource('ec2')
+        ec2 = boto3.resource('ec2', region_name=self.region)
         for sg_id in self.security_groups:
             sg = PingdomSecurityGroup(ec2.SecurityGroup(sg_id))
             sg.update_permissions(permissions)
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--region',
+        default=None,
+        help="The AWS region where the security groups are located; defaults to the environment's default region")
     parser.add_argument(
         '--whitelist',
         default='https://my.pingdom.com/probes/ipv4',
@@ -125,7 +130,7 @@ def main():
         help='One of the security groups to be updated')
     args = parser.parse_args()
 
-    updater = SecurityGroupUpdater(args.whitelist, args.protocol, args.port, args.port, getattr(args, 'security-group'))
+    updater = SecurityGroupUpdater(args.region, args.whitelist, args.protocol, args.port, args.port, getattr(args, 'security-group'))
     updater.run()
 
 if __name__ == '__main__':
